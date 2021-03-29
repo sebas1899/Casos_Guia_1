63466c6c032fec9a7dbb642ad6b5fa5e0d8edac7
sebas1899 <81540156+sebas1899@users.noreply.github.com>
2021-03-28 21:06:09 -0500

    Create Caso2.java

---
+++ src/com/senati/eti/Caso2.java
@@ -0,0 +1,23 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Definimos variable
+		int n = 0 ;
+		
+		//Lectura de variable
+		System.out.println("Ingresar el numero....: ");
+		n=sc.nextInt();
+		
+		//Impresion
+		System.out.println("------Valor Ingresado-----");
+		System.out.println("El numero es " + n);
+
+	}
+
+}
