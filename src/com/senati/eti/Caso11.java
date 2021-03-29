6df0e955e50bdffa2235430243c15f80c30cb0c2
sebas1899 <81540156+sebas1899@users.noreply.github.com>
2021-03-28 22:22:28 -0500

    Create Caso11.java

---
+++ src/com/senati/eti/Caso11.java
@@ -0,0 +1,20 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso11 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese nombre : ");
+		String nombre = sc.nextLine();
+		
+		System.out.println("-------Resultados-------");
+		System.out.println("Cantidad de Caracteres..." + nombre.length());
+		
+	
+
+	}
+
+}
