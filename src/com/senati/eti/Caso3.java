89a7f59a782f240d48cead4ecbe2cf02e9519ebd
sebas1899 <81540156+sebas1899@users.noreply.github.com>
2021-03-28 21:07:06 -0500

    Create Caso3.java

---
+++ src/com/senati/eti/Caso3.java
@@ -0,0 +1,26 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float n1 = 0 , n2 = 0 ;
+		
+		System.out.print("Ingrese primer numero......:");
+		n1= sc.nextFloat();
+		
+		System.out.print("Ingrese segundo numero.....:");
+		n2= sc.nextFloat();
+		
+		
+		System.out.println("-------Valores Ingresados-------");
+		System.out.println("Primer numero.........: " + n1);
+		System.out.println("Segundo numero........: " + n2);
+			
+		
+	}
+
+}
