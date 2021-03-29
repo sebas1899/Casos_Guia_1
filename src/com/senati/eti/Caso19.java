e5ed224e3577582dd36035e722a308a02c9dbc4f
sebas1899 <81540156+sebas1899@users.noreply.github.com>
2021-03-28 22:29:03 -0500

    Create Caso19.java

---
+++ src/com/senati/eti/Caso19.java
@@ -0,0 +1,22 @@
+package com.senati.eti;
+
+import java.util.Scanner;
+
+public class Caso19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Ingrese un lado del triangulo....:");
+		int l = sc.nextInt();
+		
+		int perimetro = 3*l ;
+		
+		System.out.println("\n-------------Resultado----------------");
+		System.out.println("El perimetro del triangulo es .......:"+perimetro);
+		
+		
+		
+	}
+
+}
